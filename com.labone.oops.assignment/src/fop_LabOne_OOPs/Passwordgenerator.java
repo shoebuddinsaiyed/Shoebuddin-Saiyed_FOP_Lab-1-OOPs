ab15df45340066e7e576f1da9acb874051205716
shoebuddinsaiyed <143965411+shoebuddinsaiyed@users.noreply.github.com>
2023-09-05 00:02:26 +0530

    Update Passwordgenerator.java

--- com.labone.oops.assignment/src/fop_LabOne_OOPs/Passwordgenerator.java
+++ com.labone.oops.assignment/src/fop_LabOne_OOPs/Passwordgenerator.java
@@ -2,14 +2,6 @@ package fop_LabOne_OOPs;
 import java.util.Random;
 
 public class Passwordgenerator {
-//public static void main(String[] args) {
-// assigning the length of password
-//int passwordLength = 7;
-// String password = passwordGenerator(passwordLength);
-
-//}
-
-//public static String passwordGenerator(int passwordLength) {
 void password_generator() {
 String Capital_Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 String Small_Letters = "abcdefghijklmnopqrstuvwxyz"; 
@@ -23,11 +15,8 @@ char[] password = new char[passwordLength];
 for (int i = 0; i<password.length; i++) {
 	password[i] = combinedvalue.charAt(random.nextInt(combinedvalue.length()));
 	}
-//return String.valueOf(password);
+
 System.out.println(String.valueOf(password));
 }
-
-
-//return combinedvalue;
 }
 
